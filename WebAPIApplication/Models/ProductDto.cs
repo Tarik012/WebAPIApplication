5c3cf55955b77b14894f45613cd86269901d91ca
Tarik012 <tc78200@gmail.com>
2024-07-29 16:27:27 +0200

    Ajout validation sur champ Name

--- WebAPIApplication/Models/ProductDto.cs
+++ WebAPIApplication/Models/ProductDto.cs
@@ -5,6 +5,7 @@ namespace WebAPIApplication.Models
     public class ProductDto
     {
         [Required]
+        [MinLength(2,ErrorMessage ="trop court !!")]
         public string Name { get; set; } = "";
         [Required]
         public string Brand {  get; set; } = "";
